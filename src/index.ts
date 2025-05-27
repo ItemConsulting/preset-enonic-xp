deb2366e0557b6e431956ab9c14474ab83b12012
Tom Arild Jakobsen <tajakobsen@gmail.com>
2025-05-27 08:38:44 +0200

    Remove @storybook/node-logger
    Relates to: #5

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import type { Configuration as WebpackConfig } from "webpack";
-import { logger } from "@storybook/node-logger";
 import { resolve } from "path";
 
 export function webpackFinal(config: WebpackConfig): WebpackConfig {
   if (config.module) {
-    logger.info("=> Enabling Freemarker Loader");
-    logger.info("=> Enabling Thymeleaf Loader");
+    console.log("=> Enabling Freemarker Loader");
+    console.log("=> Enabling Thymeleaf Loader");
 
     config.module.rules = [
       ...(config.module?.rules || []),
